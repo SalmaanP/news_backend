08a280c6e3750d45db68afcf6fdedd2e27afa2c9
Salmaan Pehlari <salmaanpehlari@gmail.com>
2017-01-01 18:32:02 -0800

    add repo

---
+++ start.py
@@ -0,0 +1,27 @@
+__author__ = 'salmaan'
+
+import NAS as c
+from time import sleep
+from datetime import datetime
+
+count = 0
+categories = ['india', 'worldnews', 'technology', 'science']
+while True:
+
+    try:
+        count += 1
+        for category in categories:
+            print "*************"
+            print str(datetime.now())
+            print "Awake: Starting now, current category: " + category + " , current count: " + str(count)
+            print "*************"
+            c.main(category)
+            print "*************"
+            print str(datetime.now())
+            print "Sleeping for 1 hour, current count: " + str(count)
+            print "*************"
+            sleep(3600)
+
+    except Exception:
+        sleep(1800)
+        continue
